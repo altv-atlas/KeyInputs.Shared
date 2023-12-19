b292f3aeb31979172b3c333f1db1c3c9c0874c10
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2023-12-19 14:36:45 +0100

    Initial Commit

---
+++ KeyInputConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace AltV.Atlas.KeyInputs.Shared;
+
+/// <summary>
+/// A static class containing all key input related constants
+/// </summary>
+public static class KeyInputConstants
+{
+    /// <summary>
+    /// The value for the key down event
+    /// </summary>
+    public const string KeyDownEventName = "atlas:keyInputs:keyDown";
+    
+    /// <summary>
+    /// The value for the key up event
+    /// </summary>
+    public const string KeyUpEventName = "atlas:keyInputs:keyUp";
+}
